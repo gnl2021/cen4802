a201362d09201b79ecc7e6cc71cd16145d50835c
gnl2021 <89617766+gnl2021@users.noreply.github.com>
2022-08-30 15:30:19 -0400

    fibonnaci added comments

--- recursivFibonnaci/src/com/company/recursivFibonnaci.java
+++ recursivFibonnaci/src/com/company/recursivFibonnaci.java
@@ -1,6 +1,21 @@
 package com.company;
 
+/**
+ * a simple java program
+ * using a recursive method
+ * to calculate the Fibonnaci sequence
+ * @author Gregory Lauture
+ * @version 1.0
+ * since 8/30/2022
+ */
 public class recursivFibonnaci {
+    /**
+     * Simple recursive method
+     * using an input number
+     * return the Fibonnaci
+     * @param n
+     * @return
+     */
     public static long recursivFib(long n){
         if ((n == 0) || (n==1))
             return n;
@@ -8,6 +23,10 @@ public class recursivFibonnaci {
             return recursivFib(n-1) + recursivFib(n-2);
     }
 
+    /**
+     * the demo main class
+     * @param args
+     */
     public static void main(String[] args) {
 	// write your code here
         long y =10;
