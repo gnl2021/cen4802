32813eeca3af8325b092a8453876e6a8bf5f491a
gnl2021 <89617766+gnl2021@users.noreply.github.com>
2022-10-12 20:01:43 -0400

    test

--- salaryInfo/src/main/java/employeeInfo.java
+++ salaryInfo/src/main/java/employeeInfo.java
@@ -1,4 +1,19 @@
 import org.apache.commons.validator.EmailValidator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.stereotype.Component;
+
+@Component
+
+// Spring jpa jars.
+@Entity
+@Table(name= "employeeInfo")
 
 public class employeeInfo {
 
