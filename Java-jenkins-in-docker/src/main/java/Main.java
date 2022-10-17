21c0a0c2aaf48f6f85f5229e2db518d4e3d52a5e
gnl2021 <89617766+gnl2021@users.noreply.github.com>
2022-10-17 01:20:53 -0400

    springmvc

--- Java-jenkins-in-docker/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static boolean checkIfInputIsAnEvenNumber(int number){
-        return number % 2 == 0;
-    }
-    public static void main(String[] args) {
-        System.out.println(checkIfInputIsAnEvenNumber(122)); // Testing in the main method
-    }
-}
